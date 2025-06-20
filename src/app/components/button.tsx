f4a82369b359ba6ba7596b58c68408102abb3b90
gleiberfelipe <gleiberfelipe@gmail.comgit config --global user.email gleiberfelipe@gmail.comgit config --global user.email gleiberfelipe@gmail.com>
2025-06-19 23:46:58 -0300

    update2.7

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -24,7 +24,7 @@ export default function CheckInButton({
       .then((data) => {
         if (data.checkedIn) {
           setDisabled(true);
-          setStatus("Você já confirmou presença!");
+          setStatus("Voce ja confirmou presenca!");
         }
       })
       .catch((error) => {
