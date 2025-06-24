edd0792c71741a64aff9e913dac7bf5ff0835383
gleiberfelipe <gleiberfelipe@gmail.comgit config --global user.email gleiberfelipe@gmail.comgit config --global user.email gleiberfelipe@gmail.com>
2025-06-24 00:01:29 -0300

    update3.5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import MobileMenu from "../app/components/menu/menu";
+import { ptBR } from "@clerk/localizations"; // ✅ correto
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider localization={ptBR}>
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
